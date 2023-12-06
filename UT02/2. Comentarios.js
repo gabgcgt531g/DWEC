e26eac502228195deb25d24310d9d4cb67ce4ae1
Gabriel González Carvajal <104269749+gabgcgt531g@users.noreply.github.com>
2023-12-06 19:17:30 +0100

    tema 2

---
+++ UT02/2. Comentarios.js
@@ -0,0 +1,19 @@
+// Este es un comentario de una línea
+var nombre="Marta" // Otro comentario sobre esta línea
+// Podemos dejar por ejemplo
+//
+// una línea en medio en blanco
+
+
+/* Ésta es una sección
+de comentarios
+en el código de JavaScript */
+/* ----------------------------------
+
+
+función imprimir()
+Imprime el listado de alumnos en orden alfabético
+-------------------------------------*/
+function imprimir() {
+    // Líneas de código JavaScript
+} 
---
+++ UT02/3. Constantes y variables.js
@@ -0,0 +1,18 @@
+//Constante
+const cajas = 3;
+
+//Variable declarada
+var edad;
+
+//Variable declarada y su valor
+var edad = 38;
+
+// Ya que no estamos obligados a declarar la variable pero es una buena práctica el hacerlo.
+edad = 38;
+// Para declarar más de una variable en la misma línea.
+var altura, peso, edad; 
+
+//Nombrar variables
+var mi_peso;
+//Esta opción es más recomendable, ya que es más cómoda de escribir.
+var miPeso;
