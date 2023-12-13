910cd5979a0b0685cf0fb1ce788b3c9312ed58f6
Gabriel González Carvajal <104269749+gabgcgt531g@users.noreply.github.com>
2023-12-13 18:26:09 +0100

    Pruebas Tarea UT02

--- UT02/Tarea UT02/script.js
+++ UT02/Tarea UT02/script.js
@@ -1,8 +1,22 @@
-function holaMundo() {
-    document.write("Hola Mundo!");
+var numTabla = window.prompt("Introduce el número de la tabla entre el 0 y el 10 ")
+var i = 1;
+if (numTabla > 10 || numTabla < 0) {
+    document.write("¡El valor debe ser entre 0 y 10!");
+} else {
+    document.write(numTabla + " x " + i + " = " + (numTabla * i) + "<br>");
+    i++;
 }
-var i;
-bucle:
-for (i = 0; i <= 5; i++) {
-    document.write(i);
-}
+
+
+/*
+let tabla = "La tabla del " + numTabla + " es:<br> <br>";
+document.write(tabla);
+
+do {
+    
+    document.write(numTabla + " x " + i + " = " + (numTabla * i) + "<br>");
+    i++;
+
+} while (i <= 10);
+document.write("<br>¡FIN DE LA TABLA DEL " + numTabla + "!");
+*/
