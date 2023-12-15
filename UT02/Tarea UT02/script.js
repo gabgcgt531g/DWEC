0d59d5714782bb5a35e53dc860e5289a6df6da34
gabgcgt531g <gabgc.z930@gmail.com>
2023-12-15 19:38:38 +0100

    Tema 2. Pruebas tarea

--- UT02/Tarea UT02/script.js
+++ UT02/Tarea UT02/script.js
@@ -1,5 +1,36 @@
-var numTabla = window.prompt("Introduce el número de la tabla entre el 0 y el 10 ")
+var numTabla = window.prompt("Introduce el número de la tabla entre el 0 y el 10");
 var i = 1;
+var res = numTabla + i;
+
+
+if (numTabla % 3 == 0) {
+    while (i <= 10 && i >= 1) {
+        var res = numTabla + i;
+        document.write(numTabla + " + " + i + " = " + (res) + " <br>");
+        i++
+    }
+
+} else if (numTabla == 2 || numTabla == 4 || numTabla) {
+    for (; i <= 10 && i >= 1;) {
+        document.write(numTabla + " x " + i + " = " + (numTabla * i) + " <br>");
+        i++
+    }
+} else {
+    document.write("--")
+}
+
+
+
+/*if (numTabla == 2 || numTabla == 4 || numTabla) {
+    document.write("Tabla del " + numTabla + " utilizando operadores artiméticos:<br><br>");
+    for (; i <= 10 && i >= 1;) {
+        document.write(numTabla + " x " + i + " = " + (numTabla * i) + " <br>");
+        i++
+    }
+} else */
+
+/*
+var numTabla = window.prompt("Introduce el número de la tabla entre el 0 y el 10 ")
 if (numTabla > 10 || numTabla < 0) {
     document.write("¡El valor debe ser entre 0 y 10!");
 } else {
@@ -7,8 +38,17 @@ if (numTabla > 10 || numTabla < 0) {
     i++;
 }
 
+else if (numTabla <= 10 && numTabla >= 1) {
+    document.write("La tabla de multiplicar del " + numTabla + " es: <br>");
+    do {
+        document.write(numTabla + " x " + i + " = " + (numTabla / i) + "<br>");
+        i++;
+    } while (i <= 10);
+} else {
+    document.write("El numero es erróneo")
+}
 
-/*
+
 let tabla = "La tabla del " + numTabla + " es:<br> <br>";
 document.write(tabla);
 
