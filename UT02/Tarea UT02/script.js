44f64b37d53f3e55c0c9e17bc0915dadc2308366
gabgcgt531g <gabgc.z930@gmail.com>
2023-12-16 09:52:01 +0100

    Fin tarea 2

--- UT02/Tarea UT02/script.js
+++ UT02/Tarea UT02/script.js
@@ -1,5 +1,5 @@
 //Pide número
-var numTabla = parseInt(prompt("Introduce un número entre el 1 y el 10"));
+var numTabla = parseInt(window.prompt("Introduce un número entre el 1 y el 10"));
 
 //Según el número elige un caso
 switch (numTabla) {
